334821889e0b6f02a23269bf29c5a9f950ac7dd5
TattooKot <kotsagastas@gmail.com>
2021-09-28 17:37:15 +0300

    Initial commit

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,45 @@
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+        TagRepository repository = new TagRepository();
+
+
+        System.out.println("Creating new tags:");
+        Tag newTag = new Tag(5, "Name1");
+        Tag newTag1 = new Tag(5, "Name2");
+        Tag newTag2 = new Tag(5, "Name3");
+        repository.create(newTag);
+        repository.create(newTag1);
+        repository.create(newTag2);
+
+
+        System.out.println("All tags:");
+        repository.getAll().forEach(System.out::println);
+        System.out.println();
+
+
+        System.out.println("First element from Tags.json");
+        System.out.println(repository.getById(1));
+        System.out.println();
+
+
+        System.out.println("Update element:");
+        System.out.println(repository.save(new Tag(3, "Vasiliy")));
+        System.out.println();
+
+        System.out.println("All tags:");
+        repository.getAll().forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Delete element with id 3:");
+        repository.deleteById(3);
+        System.out.println();
+
+        System.out.println("All tags:");
+        repository.getAll().forEach(System.out::println);
+        System.out.println();
+
+
+    }
+}
